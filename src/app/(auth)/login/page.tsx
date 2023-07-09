e3cdda68ec9418a1f6befc68bfac43c199a555ae
myxsan <yksl3461@gmail.com>
2023-07-09 23:17:21 +0300

    f

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Icons } from "@/components/Icons";
+import { Icons } from "../../../components/Icons";
 import Button from "@/components/ui/Button";
 import { signIn } from "next-auth/react";
 import { FC, useState } from "react";
